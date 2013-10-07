eabaec1927a221dae9a41f13861479201dade2d0
Tyler Schneider <teschnei@gmail.com>
2013-10-06 21:01:46 -0600

    Updated Abstract Solver Factory to raise error when methods are called

--- AbstractSolverFactory.rb
+++ AbstractSolverFactory.rb
@@ -1,3 +1,9 @@
-#!/usr/bin/env ruby
 require 'test/unit/assertions.rb'
 include Test::Unit::Assertions
+
+class AbstractSolverFactory
+
+	def self.CreateSolver(matrix);
+		raise NoMethodError, "call to abstract method 'CreateSolver'"
+	end;
+end
