5e2c7403d859b4c094e88500592841bda1f36312
cbeckett <cbeckett@ualberta.ca>
2013-10-06 21:10:17 -0600

    Added AbstractSolver and TriDiagSolver

---
+++ AbstractSolver.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+require 'test/unit/assertions.rb'
+include Test::Unit::Assertions
+
+class AbstractSolver
+	def self.solve(b)
+	end
+end
---
+++ TriDiagSolver.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+require 'test/unit/assertions.rb'
+include Test::Unit::Assertions
+
+class TriDiagSolver < AbstractSolver
+	def self.solve(b)
+	  #PRE
+      begin
+        raise "TriDiagSolver:: runtime error -> b argument must be an Array" unless b.is_a?(Array)
+      end
+	  begin
+        raise "TriDiagSolver:: runtime error -> b argument must have a size that is equal to the number of rows in A" unless (b.size == A.rowCount)
+      end
+      #PRE end
+	  
+	  #POST
+      #POST end
+	end
+end
