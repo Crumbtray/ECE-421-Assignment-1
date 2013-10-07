d47f4cb656f370c29d73b0197dd0529a2818f30e
jmorawsk <jmorawsk@ualberta.ca>
2013-10-07 07:55:30 -0600

    Update SparseMatrix.rb

--- SparseMatrix.rb
+++ SparseMatrix.rb
@@ -14,19 +14,19 @@ class SparseMatrix
 
       #POST
       begin
-        raise "SparseMatrix:: runtime error -> .new() values must be an array" unless values.kind_of?(Array)
+        raise "SparseMatrix:: runtime error -> .new() values must be an array" unless @values.kind_of?(Array)
       end
 	  
       begin
-        raise "SparseMatrix:: runtime error -> .new() value_column must be an array" unless value_column.kind_of?(Array)
+        raise "SparseMatrix:: runtime error -> .new() value_column must be an array" unless @value_column.kind_of?(Array)
       end
 	  
 	  begin
-        raise "SparseMatrix:: runtime error -> ,new() value_row must be an array" unless value_row.kind_of?(Array)
+        raise "SparseMatrix:: runtime error -> ,new() value_row must be an array" unless @value_row.kind_of?(Array)
       end
 	  
 	  begin
-        raise "SparseMatrix:: runtime error -> invalid size" unless size.is_a?(Integer) and size >= 0
+        raise "SparseMatrix:: runtime error -> invalid size" unless @size.is_a?(Integer) and @size >= 0
       end
       #POST end
 
