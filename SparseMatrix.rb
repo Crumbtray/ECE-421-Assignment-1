6ba6c1789b7588b32d0d930768cf40a819da5a10
Clinton <clint.isaac.wong@gmail.com>
2013-10-07 09:06:58 -0600

    Added GitIgnore, and modified SparseMatrix.rb

--- SparseMatrix.rb
+++ SparseMatrix.rb
@@ -5,32 +5,57 @@ include Test::Unit::Assertions
 
 class SparseMatrix
 
-   def initialize(matrix)
+    def initialize(matrix)
 
-      #PRE
-      begin
-        raise "SparseMatrix:: runtime error -> .new() matrix argument must be a Matrix" unless matrix.kind_of?(Matrix)
-      end
-      #PRE end
+        #PRE
+        begin
+            raise "SparseMatrix:: runtime error -> .new() matrix argument must be a Matrix" unless matrix.kind_of?(Matrix)
+        end
+        #PRE end
 
-      #POST
-      begin
-        raise "SparseMatrix:: runtime error -> .new() values must be an array" unless @values.kind_of?(Array)
-      end
+        #POST
+        begin
+            raise "SparseMatrix:: runtime error -> .new() values must be an array" unless @values.kind_of?(Array)
+        end
 	  
-      begin
-        raise "SparseMatrix:: runtime error -> .new() value_column must be an array" unless @value_column.kind_of?(Array)
-      end
+        begin
+            raise "SparseMatrix:: runtime error -> .new() value_column must be an array" unless @rows.kind_of?(Array)
+        end
 	  
-      begin
-        raise "SparseMatrix:: runtime error -> ,new() value_row must be an array" unless @value_row.kind_of?(Array)
-      end
+        begin
+            raise "SparseMatrix:: runtime error -> ,new() value_row must be an array" unless @columns.kind_of?(Array)
+        end
 	  
-      begin
-        raise "SparseMatrix:: runtime error -> invalid size" unless @size.is_a?(Integer) and @size >= 0
-      end
-      #POST end
+        begin
+            raise "SparseMatrix:: runtime error -> invalid size" unless @size.is_a?(Integer) and @size >= 0
+        end
+        #POST end
 
-   end
+    end
+    
+    def AddNewValue(value, col, row)
+        #PRE
+        begin
+            raise "SparseMatrix:: runtime error -> .AddNewValue() value argument must be an Integer" unless value.kind_of?(Integer)
+        end
+        
+        begin
+            raise "SparseMatrix:: runtime error -> .AddNewValue() col argument must be an Integer" unless col.kind_of?(Integer)
+        end
+        
+        begin
+            raise "SparseMatrix:: runtime error -> .AddNewValue() row argument must be an Integer" unless row.kind_of?(Integer)
+        end
+        #PRE end
+        
+        
+        #POST
+        
+        assert(@values.include(value), "Value was not inserted.")
+        assert(@rows.include(row), "Row was not inserted.")
+        assert(@columns.include(col), "Column was not inserted." )
+        
+        #POST end
+    end
 
 end
