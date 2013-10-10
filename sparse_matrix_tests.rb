749d8258cb12a505abda5c92c85914bb1646e97e
Clinton <clint.isaac.wong@gmail.com>
2013-10-10 14:27:24 -0600

    Fully tested SparseMatrix and MatrixElement data classes.
    IT WORKS HALLELUJAH
    100% CODE COMPLETION BABY

--- sparse_matrix_tests.rb
+++ sparse_matrix_tests.rb
@@ -23,8 +23,48 @@ class SparseMatrixTest < Test::Unit::TestCase
   end
   
   def test_add_value
-    m = Matrix[[0,1],[0,1]]
+    m = Matrix[[0,0],[0,1]]
     sm = SparseMatrix.new(m)
     sm.AddNewValue(1,1,0)
+    assert_equal(2, sm.elements.size)
+    a = MatrixElement.new(1,1,0)
+    assert_equal(a, sm.elements.first)
+    sm.AddNewValue(1,0,1)
+    b = MatrixElement.new(1,0,1)
+    assert(b.eql? sm.elements.first)
+  end
+  
+  def test_add_value_fail
+    m = Matrix[[0,0],[0,1]]
+    sm = SparseMatrix.new(m)
+    assert_raise ArgumentError do
+      sm.AddNewValue(0, 1, 1)
+    end
+    
+    assert_raise ArgumentError do
+      sm.AddNewValue(1, 3, 1)
+    end
+    
+    assert_raise ArgumentError do
+      sm.AddNewValue(1, 1, 3)
+    end
+  end
+  
+  def test_equality
+    m = Matrix[[0,0],[0,1]]
+    sm = SparseMatrix.new(m)
+    sa = SparseMatrix.new(m)
+    assert_raise ArgumentError do
+      sm.eql? m
+    end
+    
+    assert(sm.eql? sa)
+  end
+  
+  def test_hash
+    m = Matrix[[0,0],[0,1]]
+    sm = SparseMatrix.new(m)
+    expectedHash = sm.columnCount.hash ^ sm.rowCount.hash ^ sm.elements.hash
+    assert_equal(expectedHash, sm.hash)
   end
 end
