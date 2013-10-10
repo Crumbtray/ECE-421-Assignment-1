f03b575e58078fc6f324ffc9f6abc7a6e147f3b6
Clinton <clint.isaac.wong@gmail.com>
2013-10-10 10:21:43 -0600

    Fleshed out SparseMatrix.rb
    - Made all properties readable, but not writeable
    - Changed some runtime errors to ArgumentErrors
    - All methods implemented
    - TODO: Unit Tests

--- SparseMatrix.rb
+++ SparseMatrix.rb
@@ -4,17 +4,33 @@ require 'matrix'
 include Test::Unit::Assertions
 
 class SparseMatrix
-        
+    
+    #Properties can be read, but not written to.
+    attr_reader :values, :rows, :columns, :rowCount, :columnCount
+    
     def initialize(matrix)
 
         #PRE
         begin
-            raise "SparseMatrix:: runtime error -> .new() matrix argument must be a Matrix" unless matrix.kind_of?(Matrix)
+            raise ArgumentError, "SparseMatrix:: argument error -> .new() matrix argument must be a Matrix" unless matrix.kind_of?(Matrix)
+        end
+        #PRE end
+        
+        @values = Array.new
+        @rows = Array.new
+        @columns = Array.new
+        @rowCount = matrix.row_size
+        @columnCount = matrix.column_size
+
+        
+        matrix.each_with_index do |e, row, col|
+            if(e != 0)
+                @values.push(e)
+                @rows.push(row)
+                @columns.push(col)
+            end
         end
         
-        invariant
-        #PRE end
-
         #POST
         begin
             raise "SparseMatrix:: runtime error -> .new() values must be an array" unless @values.kind_of?(Array)
@@ -44,42 +60,48 @@ class SparseMatrix
     def AddNewValue(value, col, row)
         #PRE
         begin
-            raise "SparseMatrix:: runtime error -> .AddNewValue() value argument must be an Integer" unless value.kind_of?(Integer)
+            raise ArgumentError, "SparseMatrix:: argument error -> .AddNewValue() value argument must be an Integer" unless value.kind_of?(Integer)
         end
         
         begin
-            raise "SparseMatrix:: runtime error -> .AddNewValue() col argument must be an Integer" unless col.kind_of?(Integer)
+            raise ArgumentError, "SparseMatrix:: argument error -> .AddNewValue() col argument must be an Integer" unless col.kind_of?(Integer)
         end
         
         begin
-            raise "SparseMatrix:: runtime error -> .AddNewValue() row argument must be an Integer" unless row.kind_of?(Integer)
+            raise ArgumentError, "SparseMatrix:: argument error -> .AddNewValue() row argument must be an Integer" unless row.kind_of?(Integer)
         end
         
+        begin
+            raise ArgumentError, "SparseMatrix:: argument error -> AddNewValue() col argument must be within range" unless col < @columnCount
+        end
+        
+        begin
+            raise ArgumentError, "SparseMatrix:: argument error -> AddNewValue() row argument must be within range" unless row < @rowCount
+        end
+        
+        begin
+            raise ArgumentError, "SparseMatrix:: argument error -> AddNewValue() value argument must not be zero" unless value != 0
+        end
         invariant
         #PRE end
         
+        @values.push(value)
+        @columns.push(col)
+        @rows.push(row)
         
         #POST
-        assert(@values.include(value), "Value was not inserted.")
-        assert(@rows.include(row), "Row was not inserted.")
-        assert(@columns.include(col), "Column was not inserted." )
+        assert(@values.include?(value), "Value was not inserted.")
+        assert(@rows.include?(row), "Row was not inserted.")
+        assert(@columns.include?(col), "Column was not inserted." )
         invariant
-        #POST end
-    end
-    
-    def GetRowCount
-        #PRE
-        invariant
-            # no change
-        #PRE end
-        
-        #POST
-        invariant
-            # no change
         #POST end
     end
     
     def to_s 
+        puts "Sparse Matrix Non-Zero Valuese:"
+        for i in 0..@values.length - 1
+            puts "#{@values[i]} at #{@rows[i]}, #{@columns[i]}"
+        end
         #POST
         invariant
         #POST end
@@ -88,21 +110,44 @@ class SparseMatrix
     def equals(sm)
         #PRE
         begin
-            raise "SparseMatrix:: runtime error -> .new() matrix argument must be a Matrix" unless sm.kind_of?(SparseMatrix)
+            raise ArgumentError, "SparseMatrix:: argument error -> .new() matrix argument must be a Matrix" unless sm.kind_of?(SparseMatrix)
         end
         invariant
         #PRE end
         
-        #POST
-        invariant
-        #POST end
+        if(@rowCount != sm.rowCount)
+            puts "RowCount not the same."
+            return false
+        end
+        
+        if(@columnCount != sm.columnCount)
+            puts "ColumnCount not the same."
+            return false
+        end
+        
+        if(!@values.eql? sm.values)
+            puts "Values Array not the same."
+            return false
+        end
+        
+        if(!@rows.eql? sm.rows)
+            puts "Row Array not the same."
+            return false
+        end
+        
+        if(!@columns.eql? sm.columns)
+            puts "Columns Array not the same"
+            return false
+        end
+        
+        return true
     end
 
     def invariant
-       @rows.each{|x| assert(x <= rowCount, "Invalid row value.")}
-       @columns.each{|x| assert(x <= columnCount, "Invalid column value.")}
+       @rows.each{|x| assert(x <= @rowCount, "Invalid row value.")}
+       @columns.each{|x| assert(x <= @columnCount, "Invalid column value.")}
        assert(@rows.length >= 0, "Cannot have negative rows.")
-       assert(@coluumns.length >= 0, "Cannot have negative columns.")
+       assert(@columns.length >= 0, "Cannot have negative columns.")
        assert(@values.length >= 0, "Cannot have less than 0 values.")   
     end
     
