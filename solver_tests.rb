c5cb0a2fdaf38a4580f5321535fdbb08cad88c5e
Jason Morawski <jmorawsk@ualberta.ca>
2013-10-15 10:09:03 -0600

    Added solver tests

--- solver_tests.rb
+++ solver_tests.rb
@@ -15,11 +15,39 @@ class SolverTests < Test::Unit::TestCase
     assert_in_delta(7, answer[2], 0.00001)
   end
   
+  def test_lu_solver_wrong_b_size
+    m = Matrix[[3, -0.1, -0.2], [0.1, 7, -0.3], [0.3, -0.2, 10]]
+    sm = SparseMatrix.new(m)
+    b = [7.85, -19.3]
+    luSolver = LUSparseSolver.new(sm)
+	assert_raise ArgumentError do
+		answer = luSolver.solve(b)
+    end   
+    b = [7.85, -19.3, 71.4, 0]
+	assert_raise ArgumentError do
+		answer = luSolver.solve(b)
+    end   
+  end
+  
   def test_tridiag_solver_skeleton
     m = Matrix[[1, 4, 0, 0],[3, 4, 1,0], [0, 2, 3, 4], [0, 0, 1, 3]]
     sm = SparseMatrix.new(m)
     b = [5, 3, 4, 1]
     triSolver = TriDiagSolver.new(sm)
     answer = triSolver.solve(b)
+  end
+  
+  def test_tridiag_solver_wrong_b_size
+    m = Matrix[[1, 4, 0, 0],[3, 4, 1,0], [0, 2, 3, 4], [0, 0, 1, 3]]
+    sm = SparseMatrix.new(m)
+    b = [5, 3, 4]
+    triSolver = TriDiagSolver.new(sm)
+	assert_raise ArgumentError do
+		answer = triSolver.solve(b)
+    end
+    b = [5, 3, 4, 1, 0]
+	assert_raise ArgumentError do
+		answer = triSolver.solve(b)
+    end
   end
 end
